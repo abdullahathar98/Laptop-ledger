84c1e877d040971fac54a4cefe16376d7e0d00ab
abdullah athar <65698046+abdullahathar98@users.noreply.github.com>
2026-08-15 01:03:08 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'laptop-ledger-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', function(event){
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache){ return cache.addAll(ASSETS); })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event){
+  event.waitUntil(
+    caches.keys().then(function(names){
+      return Promise.all(
+        names.filter(function(n){ return n !== CACHE_NAME; }).map(function(n){ return caches.delete(n); })
+      );
+    }).then(function(){ return self.clients.claim(); })
+  );
+});
+
+self.addEventListener('fetch', function(event){
+  event.respondWith(
+    fetch(event.request).then(function(res){
+      var resClone = res.clone();
+      caches.open(CACHE_NAME).then(function(cache){ cache.put(event.request, resClone); });
+      return res;
+    }).catch(function(){
+      return caches.match(event.request);
+    })
+  );
+});
